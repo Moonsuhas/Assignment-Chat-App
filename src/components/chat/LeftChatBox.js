bbcb054d3bc3722576adc1ac1e3feb426a2fcbe0
Suhas Moon <suhasmoon11@gmail.com>
2023-09-02 09:04:01 +0530

    Updated the CSS and LeftChatBox.js files

--- src/components/chat/LeftChatBox.js
+++ src/components/chat/LeftChatBox.js
@@ -38,7 +38,7 @@ function LeftChatBox(props) {
             <BsFillChatLeftDotsFill className=" ms-3 text-primary h4" />
 
             {/* github Link  */}
-            <a href="https://github.com/SUSOBHANLAL/CHAT-APP.github.io" target="_blank" rel="noreferrer">< BsGithub className="ms-3 text-primary h3" /> </a>
+            <a href="https://github.com/Moonsuhas" target="_blank" rel="noreferrer">< BsGithub className="ms-3 text-primary h3" /> </a>
           </div>
         </div>
         {/* <!-- **********************  search Bar  ******************* --> */}
